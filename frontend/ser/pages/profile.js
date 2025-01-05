83c9675d3959fe8ee91c5e42ffbe13325ef9f9e4
Ze0ro99 <146000493+Ze0ro99@users.noreply.github.com>
2025-01-05 18:26:43 +0300

    Update

---
+++ frontend/ser/pages/profile.js
@@ -0,0 +1,14 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const Profile = () => {
+  return (
+    <div>
+      <Header />
+      <h2>Your Profile</h2>
+      <Footer />
+    </div>
+  );
+};
+
+export default Profile;
