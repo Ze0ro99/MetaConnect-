b67d9941ec895f0196ed159df33fb2e73b1c8a4a
Ze0ro99 <146000493+Ze0ro99@users.noreply.github.com>
2025-01-05 18:25:57 +0300

    Update

---
+++ frontend/ser/pages/index.js
@@ -0,0 +1,14 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <h2>Welcome to MetaConnect!</h2>
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
