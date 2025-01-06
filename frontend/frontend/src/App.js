b4c93a6b5982803b44f13a45d20df803c8b6d911
Ze0ro99 <146000493+Ze0ro99@users.noreply.github.com>
2025-01-06 07:32:35 +0300

    Update frontend

---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Connect from './Connect';
+
+function App() {
+  return (
+    <div className="App">
+      <Connect />
+    </div>
+  );
+}
+
+export default App;
