01bc9056c5dd90c83fc0b20f32f7efe76e73bdc5
Ze0ro99 <146000493+Ze0ro99@users.noreply.github.com>
2025-01-06 03:32:37 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <SpeedInsights
-        / يمكنك إضافة الخيارات المطلوبة هنا
-        apiKey="5d8bc63cfa473df9a90c3308432e255c892f4b363143dfd8fa98c6c44d1b0bda13b78ce5a9c195b1648220c33a7b4f026b8176c0a0c87ba421c309533a57480c" / تأكد من استبدال YOUR_API_KEY بمفتاح API الخاص بك
+        // يمكنك إضافة الخيارات المطلوبة هنا
+        apiKey="5d8bc63cfa473df9a90c3308432e255c892f4b363143dfd8fa98c6c44d1b0bda13b78ce5a9c195b1648220c33a7b4f026b8176c0a0c87ba421c309533a57480c" // تأكد من استبدال YOUR_API_KEY بمفتاح API الخاص بك
       />
       <Component {...pageProps} />
     </>
