3f36545aa20d3e73e27d7d44a2da95538e430991
Ze0ro99 <146000493+Ze0ro99@users.noreply.github.com>
2025-01-06 08:17:52 +0300

    Update App.js

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Connect from './Connect';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <div className="App">
       <Connect />
+      <Analytics /> {/* إضافة مكون Analytics هنا */}
     </div>
   );
 }
