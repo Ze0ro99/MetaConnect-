9f7f54a9d15b1cc16d80d4fa02c16b8f1cc02ec0
Ze0ro99 <146000493+Ze0ro99@users.noreply.github.com>
2025-01-08 14:54:39 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
+// pages/_app.js
+
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <SpeedInsights apiKey="5d8bc63cfa473df9a90c3308432e255c892f4b363143dfd8fa98c6c44d1b0bda13b78ce5a9c195b1648220c33a7b4f026b8176c0a0c87ba421c309533a57480c" />
+      {/* تأكد من أن لديك مفتاح API في متغير البيئة */}
+      <SpeedInsights apiKey={process.env.NEXT_PUBLIC_SPEED_INSIGHTS_API_KEY} />
       <Component {...pageProps} />
     </>
   );
